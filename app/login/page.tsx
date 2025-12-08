182f73b1b47c7a33e61765bf998a97ef546af967
Lokesh-Kanna-V <vlokeshkanna@gmail.com>
2025-12-08 17:30:17 +0530

    LK_0812_1730

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+//? Import UI
+import LoginUI from "@/features/login/ui/loginUI";
+
+export default function Login() {
+  return (
+    <div>
+      <LoginUI />
+    </div>
+  );
+}
---
+++ features/login/ui/loginUI.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+//? Module Imports
+import LoginForm from "../modules/login_form";
+
+export default function LoginUI() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
